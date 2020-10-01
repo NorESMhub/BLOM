a3f9ea5a5ab664cf94827957212ae1339dea00fd
Tomas Torsvik <tomas.torsvik.work@gmail.com>
2020-10-01 10:34:28 +0200

    powadi.F90 code refactoring
    - Replace all DO ... CONTINUE blocks with DO ... ENDDO
    - Set intent for variables passed to subroutine
    - Replace F77-style relational operators with symbolic equivalents
    - Replace fixed-form with free-form spacing convention

--- hamocc/powadi.F90
+++ hamocc/powadi.F90
@@ -17,7 +17,7 @@
 ! along with BLOM. If not, see https://www.gnu.org/licenses/.
 
 
-      SUBROUTINE POWADI(j,kpie,kpje,solrat,sedb1,sediso,bolven,omask)
+subroutine powadi(j,kpie,kpje,solrat,sedb1,sediso,bolven,omask)
 !**********************************************************************
 !
 !**** *POWADI* - vertical diffusion with simultaneous dissolution.
@@ -56,79 +56,86 @@
 !
 !**********************************************************************
 
-      USE mo_carbch
-      USE mo_sedmnt
-      USE mo_biomod
-      use mo_param1_bgc 
-      USE mo_control_bgc
-      use mo_vgrid, only: bolay
+  use mo_carbch
+  use mo_sedmnt
+  use mo_biomod
+  use mo_param1_bgc
+  use mo_control_bgc
+  use mo_vgrid, only: bolay
 
-      implicit none
+  implicit none
 
-      REAL :: sedb1(kpie,0:ks),sediso(kpie,0:ks)
-      REAL :: solrat(kpie,ks)
-      REAL :: bolven(kpie)
-      REAL :: TREDSY(kpie,0:ks,3)
-      REAL :: omask(kpie,kpje)
-      INTEGER :: kpie,kpje,i,j,k,l
-      REAL :: asu,alo
-      
-      DO 1321 k=1,ks
-         asu=sedict*seddzi(k)*porwah(k)
-         alo=0.
-         IF(k.LT.ks)alo=sedict*seddzi(k+1)*porwah(k+1)
-         DO 1321 i=1,kpie
-            tredsy(i,k,1) = -asu
-            tredsy(i,k,3) = -alo 
-            tredsy(i,k,2) =  seddw(k)*porwat(k) - tredsy(i,k,1)       &
-     &          - tredsy(i,k,3) + solrat(i,k)*porwat(k)*seddw(k)
- 1321    CONTINUE
+  integer, intent(in) :: j, kpie, kpje
+  real, dimension(kpie,ks),   intent(in)    :: solrat
+  real, dimension(kpie,0:ks), intent(inout) :: sedb1, sediso
+  real, dimension(kpie),      intent(in)    :: bolven
+  real, dimension(kpie,kpje), intent(in)    :: omask
 
-         k=0
-         asu=0.
-         alo=sedict*seddzi(1)*porwah(1)
-         DO 1421 i=1,kpie
-          IF(omask(i,j).GT.0.5) THEN
-              tredsy(i,k,1) = -asu
-              tredsy(i,k,3) = -alo 
-              tredsy(i,k,2) = bolven(i)*bolay(i,j)                   &
-     &                        -tredsy(i,k,1)-tredsy(i,k,3)
-          ELSE
-              tredsy(i,k,1) = 0
-              tredsy(i,k,3) = 0
-              tredsy(i,k,2) = 0
-          ENDIF
-	  
-1421  CONTINUE
+  ! Local variables
+  integer :: i,k,l
+  real :: asu, alo
+  real, dimension(kpie,0:ks,3) :: tredsy
 
-       DO 132 K=1,KS
-          DO 133 i=1,kpie
-         IF(omask(i,j).GT.0.5) THEN
-                tredsy(i,k-1,1) = tredsy(i,k,1) / tredsy(i,k-1,2)
-                tredsy(i,k,2)   = tredsy(i,k,2)                       &
-     &              - tredsy(i,k-1,3) * tredsy(i,k,1) / tredsy(i,k-1,2)
-             ENDIF
- 133      CONTINUE
- 132   CONTINUE
- 
-       DO 135 k=1,ks
-       DO 135 i=1,kpie
-          sedb1(i,k) = sedb1(i,k) - tredsy(i,k-1,1) * sedb1(i,k-1)
-135    CONTINUE
+!**********************************************************************
 
-       k=ks
-       DO 136 i=1,kpie
-          IF(omask(i,j).GT.0.5) sediso(i,k) = sedb1(i,k) / tredsy(i,k,2)
-136    CONTINUE
+  do k = 1, ks
+     asu = sedict * seddzi(k) * porwah(k)
+     alo = 0.
+     if(k < ks) alo = sedict * seddzi(k+1) * porwah(k+1)
+     do i = 1, kpie
+        tredsy(i,k,1) = -asu
+        tredsy(i,k,3) = -alo
+        tredsy(i,k,2) = seddw(k) * porwat(k) - tredsy(i,k,1)                   &
+             &          - tredsy(i,k,3) + solrat(i,k) * porwat(k) * seddw(k)
+     enddo
+  enddo
 
-       DO 137 k=1,ks
-          l=ks-k
-          DO 137 i=1,kpie
-         IF(omask(i,j).GT.0.5) sediso(i,l) =                           &
-     &           ( sedb1(i,l) - tredsy(i,l,3) * sediso(i,l+1) )       &
-     &           / tredsy(i,l,2)
- 137      CONTINUE
+  k = 0
+  asu = 0.
+  alo = sedict * seddzi(1) * porwah(1)
+  do i = 1, kpie
+     if(omask(i,j) > 0.5) then
+        tredsy(i,k,1) = -asu
+        tredsy(i,k,3) = -alo
+        tredsy(i,k,2) = bolven(i) * bolay(i,j) - tredsy(i,k,1)                 &
+             &          - tredsy(i,k,3)
+     else
+        tredsy(i,k,1) = 0
+        tredsy(i,k,3) = 0
+        tredsy(i,k,2) = 0
+     endif
+  enddo
 
+  do k = 1, ks
+     do i = 1, kpie
+        if(omask(i,j) > 0.5) then
+           tredsy(i,k-1,1) = tredsy(i,k,1) / tredsy(i,k-1,2)
+           tredsy(i,k,2)   = tredsy(i,k,2)                                     &
+                &            - tredsy(i,k-1,3) * tredsy(i,k,1) / tredsy(i,k-1,2)
+        endif
+     enddo
+  enddo
 
-      RETURN
-      END
+  do k = 1, ks
+     do i = 1, kpie
+        sedb1(i,k) = sedb1(i,k) - tredsy(i,k-1,1) * sedb1(i,k-1)
+     enddo
+  enddo
+
+  k = ks
+  do i = 1, kpie
+     if(omask(i,j) > 0.5) sediso(i,k) = sedb1(i,k) / tredsy(i,k,2)
+  enddo
+
+  do k = 1, ks
+     l = ks - k
+     do i = 1, kpie
+        if(omask(i,j) > 0.5) then
+           sediso(i,l) = ( sedb1(i,l) - tredsy(i,l,3) * sediso(i,l+1) )        &
+                &        / tredsy(i,l,2)
+        endif
+     enddo
+  enddo
+
+  return
+end subroutine powadi
