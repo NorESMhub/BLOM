3c707403b384280bd2dc0e364d60c62802d42790
Tomas Torsvik <tomas.torsvik@uib.no>
2022-05-09 09:17:14 +0200

    Remove reference to vertical coordinate, and set parameter for 12 month pi_ph record.

--- hamocc/mo_dmsph.F90
+++ hamocc/mo_dmsph.F90
@@ -3,16 +3,16 @@
 ! This file is part of BLOM/iHAMOCC.
 !
 ! BLOM is free software: you can redistribute it and/or modify it under the
-! terms of the GNU Lesser General Public License as published by the Free 
-! Software Foundation, either version 3 of the License, or (at your option) 
-! any later version. 
+! terms of the GNU Lesser General Public License as published by the Free
+! Software Foundation, either version 3 of the License, or (at your option)
+! any later version.
 !
-! BLOM is distributed in the hope that it will be useful, but WITHOUT ANY 
-! WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+! BLOM is distributed in the hope that it will be useful, but WITHOUT ANY
+! WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 ! FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
-! more details. 
+! more details.
 !
-! You should have received a copy of the GNU Lesser General Public License 
+! You should have received a copy of the GNU Lesser General Public License
 ! along with BLOM. If not, see https://www.gnu.org/licenses/.
 
 module mo_dmsph
@@ -21,43 +21,48 @@ module mo_dmsph
   private
   public :: get_pi_ph,get_dmsph,pi_ph_path,with_dmsph
 
-  ! Path to input data, set through namelist 
-  ! in hamocc_init.F 
-  character(len=256),save    :: pi_ph_path = ''
-
   ! Activate/deactivate calculation of DMS as a function of pH.
   logical :: with_dmsph = .false.
 
-  ! pH field
-  real, parameter :: dms_gamma = 0.87 
+  ! Path to input data, set through namelist in hamocc_init.F
+  character(len=256),save    :: pi_ph_path = ''
+
+  ! Length of surface PI pH record from file
+  ! Assume monthly record by default (otherwise change get_dmsph)
+  integer, parameter :: pi_ph_record = 12
+
+  ! dms_ph scaling factor
+  real, parameter :: dms_gamma = 0.87
+
+  ! surface PI pH field
   real, dimension(:,:,:), allocatable :: pi_ph
 
-CONTAINS     
+CONTAINS
 
 
-  SUBROUTINE GET_PI_PH(kpie,kpje,kpke,omask)
+  SUBROUTINE GET_PI_PH(kpie,kpje,omask)
   !**********************************************************************
 
-    use mo_control_bgc, only: io_stdo_bgc 
-    use netcdf,         only: nf90_noerr,nf90_nowrite,nf90_close,nf90_open 
+    use mo_control_bgc, only: io_stdo_bgc
+    use netcdf,         only: nf90_noerr,nf90_nowrite,nf90_close,nf90_open
     use mod_xc,         only: mnproc,xchalt
 
     implicit none
-    INTEGER, INTENT(in) :: kpie,kpje,kpke
+    INTEGER, INTENT(in) :: kpie,kpje
     INTEGER ::i,j,l
-  
+
     REAL,intent(in) ::omask(kpie,kpje)
 
     ! define the fields
-    REAL :: pi_ph_in(kpie,kpje,12)
+    REAL :: pi_ph_in(kpie,kpje,pi_ph_record)
     INTEGER ncid,ncstat
 
     ! allocate pi_ph field
-    if(.not. allocated(pi_ph)) call alloc_pi_ph(kpie,kpje,kpke)
+    if(.not. allocated(pi_ph)) call alloc_pi_ph(kpie,kpje)
 
     !
     ! Open netCDF data file
-    !      
+    !
     IF(mnproc==1) THEN
        ncstat = NF90_OPEN(trim(pi_ph_path)//'MONTHLY_PI_PH.nc',        &
      &                   NF90_NOWRITE, ncid)
@@ -69,7 +74,7 @@ CONTAINS
     END IF
     !
     ! Read  data
-    call read_netcdf_var(ncid,'pH',pi_ph_in(1,1,1),12,0,0)
+    call read_netcdf_var(ncid,'pH',pi_ph_in(1,1,1),pi_ph_record,0,0)
     !
     ! Close file
     IF(mnproc==1) THEN
@@ -81,12 +86,12 @@ CONTAINS
     END IF
 
     ! set missings over land
-    do l=1,12
+    do l=1,pi_ph_record
        do j=1,kpje
           do i=1,kpie
-             if(omask(i,j).gt.0.5) then 
+             if(omask(i,j).gt.0.5) then
                 pi_ph(i,j,l) = pi_ph_in(i,j,l)
-             else 
+             else
                 pi_ph(i,j,l) = 0.
              endif
           enddo
@@ -99,22 +104,22 @@ CONTAINS
   !**********************************************************************
   ! Allocate the PI_PH field.
   !**********************************************************************
-  subroutine alloc_pi_ph(kpie,kpje,kpke)
+  subroutine alloc_pi_ph(kpie,kpje)
     use mod_xc,         only: mnproc
     use mo_control_bgc, only: io_stdo_bgc
-    
+
     implicit none
-    integer, intent(in) :: kpie,kpje,kpke
+    integer, intent(in) :: kpie,kpje
     integer             :: errstat
 
     IF (mnproc.eq.1) THEN
        WRITE(io_stdo_bgc,*)'Memory allocation for variable pi_ph ...'
        WRITE(io_stdo_bgc,*)'First dimension    : ',kpie
        WRITE(io_stdo_bgc,*)'Second dimension   : ',kpje
-       WRITE(io_stdo_bgc,*)'Third dimension    : ',kpke
+       WRITE(io_stdo_bgc,*)'Third dimension    : ',pi_ph_record
     ENDIF
 
-    ALLOCATE (pi_ph(kpie,kpje,kpke),stat=errstat)
+    ALLOCATE (pi_ph(kpie,kpje,pi_ph_record),stat=errstat)
     if(errstat.ne.0) stop 'not enough memory pi_ph'
     pi_ph(:,:,:) = 0.0
 
