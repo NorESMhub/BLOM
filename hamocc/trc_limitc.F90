65ee7696b55d4cd1905bf3bcb2801907ee75b0b6
Tomas Torsvik <tomas.torsvik.work@gmail.com>
2022-08-19 21:05:59 +0200

    Reformat hamocc/trc_limitc.F90 according to free form convention.

--- hamocc/trc_limitc.F90
+++ hamocc/trc_limitc.F90
@@ -1,136 +1,132 @@
-c Copyright (C) 2020  J. Schwinger, M. Bentsen
-c
-c This file is part of BLOM/iHAMOCC.
-c
-c BLOM is free software: you can redistribute it and/or modify it under the
-c terms of the GNU Lesser General Public License as published by the Free 
-c Software Foundation, either version 3 of the License, or (at your option) 
-c any later version. 
-c
-c BLOM is distributed in the hope that it will be useful, but WITHOUT ANY 
-c WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
-c FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
-c more details. 
-c
-c You should have received a copy of the GNU Lesser General Public License 
-c along with BLOM. If not, see https://www.gnu.org/licenses/.
+! Copyright (C) 2020  J. Schwinger, M. Bentsen
+!
+! This file is part of BLOM/iHAMOCC.
+!
+! BLOM is free software: you can redistribute it and/or modify it under the
+! terms of the GNU Lesser General Public License as published by the Free
+! Software Foundation, either version 3 of the License, or (at your option)
+! any later version.
+!
+! BLOM is distributed in the hope that it will be useful, but WITHOUT ANY
+! WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+! FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+! more details.
+!
+! You should have received a copy of the GNU Lesser General Public License
+! along with BLOM. If not, see https://www.gnu.org/licenses/.
 
 
-      subroutine trc_limitc(nn)
-c***********************************************************************
-c
-c**** *SUBROUTINE trc_limitc* - remove negative tracer values.
-c
-c     J. Schwinger      *GFI, UiB        initial version, 2014-06-17
-c      - 
-c
-c     Modified
-c     --------
-c     J.Schwinger,      *Uni Research, Bergen*   2018-04-12
-c     - fixed a bug related to the 2 time-level scheme
-c
-c
-c  
-c     Purpose
-c     -------
-c      Remove negative tracer values in the first layer in a mass
-c      conservative fashion (i.e. the mass deficit removed is 
-c      transfered to non-negative points by a multiplicative 
-c      correction). This is done since the virtual tracer fluxes
-c      (applied in mxlayr.F directly before HAMOCC is called) can
-c      cause negative tracer values in regions with low concentration
-c      and strong precipitation.
-c
-c***********************************************************************
-c
-      use mod_xc,      only: ii,jj,ips,ifp,isp,ilp,xcsum
-      use mod_grid,    only: scp2
-      use mod_state,   only: dp
-      use mod_tracers, only: ntrbgc, itrbgc, trc
-      use mod_utility, only: util1
-c
-      implicit none
-c
-      integer       :: nn
-      integer       :: i,j,l,nt,kn
-      real          :: trbudo(ntrbgc),trbudn,q
-c
-c --- ------------------------------------------------------------------
-c --- - compute tracer budgets before removing negative values
-c --- ------------------------------------------------------------------
-c
-      kn=1+nn
-c
-      do nt=1,ntrbgc
-c
-        util1(:,:)=0.
-c
-c$OMP PARALLEL DO PRIVATE(l,i)
-        do j=1,jj
-          do l=1,isp(j)
-          do i=max(1,ifp(j,l)),min(ii,ilp(j,l))
-            util1(i,j)=util1(i,j)
-     .                +trc(i,j,kn,itrbgc+nt-1)*dp(i,j,kn)*scp2(i,j)
-          enddo
-          enddo
+subroutine trc_limitc(nn)
+!***********************************************************************
+!
+!**** *SUBROUTINE trc_limitc* - remove negative tracer values.
+!
+!     J. Schwinger      *GFI, UiB        initial version, 2014-06-17
+!      -
+!
+!     Modified
+!     --------
+!     J.Schwinger,      *Uni Research, Bergen*   2018-04-12
+!     - fixed a bug related to the 2 time-level scheme
+!
+!
+!
+!     Purpose
+!     -------
+!      Remove negative tracer values in the first layer in a mass
+!      conservative fashion (i.e. the mass deficit removed is
+!      transfered to non-negative points by a multiplicative
+!      correction). This is done since the virtual tracer fluxes
+!      (applied in mxlayr.F directly before HAMOCC is called) can
+!      cause negative tracer values in regions with low concentration
+!      and strong precipitation.
+!
+!***********************************************************************
+  use mod_xc,      only: ii,jj,ips,ifp,isp,ilp,xcsum
+  use mod_grid,    only: scp2
+  use mod_state,   only: dp
+  use mod_tracers, only: ntrbgc, itrbgc, trc
+  use mod_utility, only: util1
+
+  implicit none
+
+  integer       :: nn
+  integer       :: i,j,l,nt,kn
+  real          :: trbudo(ntrbgc),trbudn,q
+
+  ! --- ------------------------------------------------------------------
+  ! --- - compute tracer budgets before removing negative values
+  ! --- ------------------------------------------------------------------
+
+  kn=1+nn
+
+  do nt=1,ntrbgc
+
+     util1(:,:)=0.
+
+!$OMP PARALLEL DO PRIVATE(l,i)
+     do j=1,jj
+        do l=1,isp(j)
+           do i=max(1,ifp(j,l)),min(ii,ilp(j,l))
+              util1(i,j) = util1(i,j)                                    &
+                   &       +trc(i,j,kn,itrbgc+nt-1)*dp(i,j,kn)*scp2(i,j)
+           enddo
         enddo
-c$OMP END PARALLEL DO
-c
-        call xcsum(trbudo(nt),util1,ips)
-c
-      enddo
-c
-c
-c --- ------------------------------------------------------------------
-c --- - remove negative tracer values in the surface layer
-c --- ------------------------------------------------------------------
-c
-c$OMP PARALLEL DO PRIVATE(j,l,i)
-      do nt=itrbgc,itrbgc+ntrbgc-1
-        do j=1,jj
-          do l=1,isp(j)
-          do i=max(1,ifp(j,l)),min(ii,ilp(j,l))
+     enddo
+!$OMP END PARALLEL DO
+
+     call xcsum(trbudo(nt),util1,ips)
+
+  enddo
+
+  ! --- ------------------------------------------------------------------
+  ! --- - remove negative tracer values in the surface layer
+  ! --- ------------------------------------------------------------------
+
+!$OMP PARALLEL DO PRIVATE(j,l,i)
+  do nt=itrbgc,itrbgc+ntrbgc-1
+     do j=1,jj
+        do l=1,isp(j)
+           do i=max(1,ifp(j,l)),min(ii,ilp(j,l))
               trc(i,j,kn,nt) = max(trc(i,j,kn,nt),0.0)
-          enddo
-          enddo
+           enddo
         enddo
-      enddo
-c$OMP END PARALLEL DO
-c
-c
-c --- ------------------------------------------------------------------
-c --- - recalculate and correct tracer budgets 
-c --- ------------------------------------------------------------------
-c
-      do nt=1,ntrbgc
-c
-        util1(:,:)=0.
-c
-c$OMP PARALLEL DO PRIVATE(l,i)
-        do j=1,jj
-          do l=1,isp(j)
-          do i=max(1,ifp(j,l)),min(ii,ilp(j,l))
-            util1(i,j)=util1(i,j)
-     .                +trc(i,j,kn,itrbgc+nt-1)*dp(i,j,kn)*scp2(i,j)
-          enddo
-          enddo
+     enddo
+  enddo
+!$OMP END PARALLEL DO
+
+  ! --- ------------------------------------------------------------------
+  ! --- - recalculate and correct tracer budgets
+  ! --- ------------------------------------------------------------------
+
+  do nt=1,ntrbgc
+
+     util1(:,:)=0.
+
+!$OMP PARALLEL DO PRIVATE(l,i)
+     do j=1,jj
+        do l=1,isp(j)
+           do i=max(1,ifp(j,l)),min(ii,ilp(j,l))
+              util1(i,j) = util1(i,j)                                    &
+                   &       +trc(i,j,kn,itrbgc+nt-1)*dp(i,j,kn)*scp2(i,j)
+           enddo
         enddo
-c$OMP END PARALLEL DO
-c
-        call xcsum(trbudn,util1,ips)
-        q=trbudo(nt)/max(1.e-14,trbudn)
-c
-c$OMP PARALLEL DO PRIVATE(l,i)
-        do j=1,jj
-          do l=1,isp(j)
-          do i=max(1,ifp(j,l)),min(ii,ilp(j,l))
-            trc(i,j,kn,itrbgc+nt-1) = trc(i,j,kn,itrbgc+nt-1)*q
-          enddo
-          enddo
+     enddo
+!$OMP END PARALLEL DO
+
+     call xcsum(trbudn,util1,ips)
+     q = trbudo(nt)/max(1.e-14,trbudn)
+
+!$OMP PARALLEL DO PRIVATE(l,i)
+     do j=1,jj
+        do l=1,isp(j)
+           do i=max(1,ifp(j,l)),min(ii,ilp(j,l))
+              trc(i,j,kn,itrbgc+nt-1) = trc(i,j,kn,itrbgc+nt-1)*q
+           enddo
         enddo
-c$OMP END PARALLEL DO
-c
-      enddo
-c
-      return
-      end
+     enddo
+!$OMP END PARALLEL DO
+
+  enddo
+
+end subroutine trc_limitc
