f53d085460f1cfa274ef5638f27a7b38a8865065
Tomas Torsvik <tomas.torsvik.work@gmail.com>
2022-08-19 20:46:04 +0200

    Reformat hamocc/restart_hamoccwt.F90 following free form convention.

--- hamocc/restart_hamoccwt.F90
+++ hamocc/restart_hamoccwt.F90
@@ -1,37 +1,36 @@
-c Copyright (C) 2020  K. Assmann, J. Tjiputra, J. Schwinger, M. Bentsen
-c
-c This file is part of BLOM/iHAMOCC.
-c
-c BLOM is free software: you can redistribute it and/or modify it under the
-c terms of the GNU Lesser General Public License as published by the Free 
-c Software Foundation, either version 3 of the License, or (at your option) 
-c any later version. 
-c
-c BLOM is distributed in the hope that it will be useful, but WITHOUT ANY 
-c WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
-c FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
-c more details. 
-c
-c You should have received a copy of the GNU Lesser General Public License 
-c along with BLOM. If not, see https://www.gnu.org/licenses/.
+! Copyright (C) 2020  K. Assmann, J. Tjiputra, J. Schwinger, M. Bentsen
+!
+! This file is part of BLOM/iHAMOCC.
+!
+! BLOM is free software: you can redistribute it and/or modify it under the
+! terms of the GNU Lesser General Public License as published by the Free
+! Software Foundation, either version 3 of the License, or (at your option)
+! any later version.
+!
+! BLOM is distributed in the hope that it will be useful, but WITHOUT ANY
+! WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+! FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+! more details.
+!
+! You should have received a copy of the GNU Lesser General Public License
+! along with BLOM. If not, see https://www.gnu.org/licenses/.
 
 
-      subroutine restart_hamoccwt(rstfnm_ocn)
-c
-c write restart for HAMOCC
-c
-      use mod_time,      only: date,nstep
-      use mod_xc,        only: idm,jdm,kdm 
-      use mod_tracers,   only: ntrbgc,ntr,itrbgc,trc
-      use mo_intfcblom,  only: omask
-c
-      implicit none
-c
-      character(len=*) :: rstfnm_ocn
+subroutine restart_hamoccwt(rstfnm_ocn)
+!
+! write restart for HAMOCC
+!
+  use mod_time,      only: date,nstep
+  use mod_xc,        only: idm,jdm,kdm
+  use mod_tracers,   only: ntrbgc,ntr,itrbgc,trc
+  use mo_intfcblom,  only: omask
 
-      CALL AUFW_BGC(idm,jdm,kdm,ntr,ntrbgc,itrbgc,trc
-     .             ,date%year,date%month,date%day,nstep,omask
-     .             ,rstfnm_ocn)
-c
-      return
-      end
+  implicit none
+
+  character(len=*) :: rstfnm_ocn
+
+  CALL AUFW_BGC(idm,jdm,kdm,ntr,ntrbgc,itrbgc,trc           &
+       &       ,date%year,date%month,date%day,nstep,omask   &
+       &       ,rstfnm_ocn)
+
+end subroutine restart_hamoccwt
