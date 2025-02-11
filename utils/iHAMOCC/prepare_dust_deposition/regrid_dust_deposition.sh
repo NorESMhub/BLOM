e7380463f4874c548e58717c82d33e2f64bc30c8
jmaerz <92309038+jmaerz@users.noreply.github.com>
2025-02-11 18:09:09 +0100

    generalize script further

--- utils/iHAMOCC/prepare_dust_deposition/regrid_dust_deposition.sh
+++ utils/iHAMOCC/prepare_dust_deposition/regrid_dust_deposition.sh
@@ -2,10 +2,27 @@
 
 module load CDO/2.0.6-gompi-2022a
 module load NCO/5.1.9-iomkl-2022a
+version=$(date '+%Y%m%d')
 
-GRIDFILE="/cluster/shared/noresm/inputdata/ocn/blom/grid/grid_tnx0.5v1_20240702.nc"
 GRID="tnx0.5v1"
 DUST_IN="dustdep_mhw2006_T42.nc"
+
+basepath="/cluster/shared/noresm/inputdata/ocn/blom/grid/"
+
+echo "$GRID"
+if [ ${GRID} == 'tnx2v1' ]; then
+  GRIDFILE="grid_${GRID}_20130206.nc"
+elif [ ${GRID} == 'tnx1v4' ]; then
+  GRIDFILE="grid_${GRID}_20170622.nc"
+elif [ ${GRID} == 'tnx0.5v1' ]; then
+  GRIDFILE="grid_tnx0.5v1_20240702.nc"
+elif [ ${GRID} == 'tnx0.25v4' ]; then
+  GRIDFILE="grid_tnx0.25v4_20170622.nc"
+elif [ ${GRID} == 'tnx0.125v4' ]; then
+  GRIDFILE="grid_tnx0.125v4_20221013.nc"
+fi
+
+GRIDFILE=${basepath}/${GRIDFILE}
 
 #-----------------------------------------------------------------generate gridfile:
 # get the respective variables:
@@ -30,11 +47,15 @@ cdo gencon,gridfile_${GRID}.nc ${DUST_IN} weights_${GRID}.nc
 cdo -setattribute,DUST@units="kg/m2/month" \
     -setattribute,DUST@long_name="dust_deposition" \
     -setattribute,DUST@coordinates="plat plon"  \
-    -setname,DUST -mulc,2592000 -remap,gridfile_${GRID}.nc,weights_${GRID}.nc  $DUST_IN dustdep_mhw2006_tnx0.5v1_20250210.nc
+    -setname,DUST -mulc,2592000 -remap,gridfile_${GRID}.nc,weights_${GRID}.nc $DUST_IN dustdep_mhw2006_${GRID}_${version}.nc
 
-ncrename -h -O -v lat,plat dustdep_mhw2006_tnx0.5v1_20250210.nc
-ncrename -h -O -v lon,plon dustdep_mhw2006_tnx0.5v1_20250210.nc
-ncrename -h -O -v lon_bnds,plon_bnds dustdep_mhw2006_tnx0.5v1_20250210.nc
-ncrename -h -O -v lat_bnds,plat_bnds dustdep_mhw2006_tnx0.5v1_20250210.nc
-ncatted -a bounds,plat,o,c,"plat_bnds" dustdep_mhw2006_tnx0.5v1_20250210.nc
-ncatted -a bounds,plon,o,c,"plon_bnds" dustdep_mhw2006_tnx0.5v1_20250210.nc
+ncrename -h -O -v lat,plat dustdep_mhw2006_${GRID}_${version}.nc
+ncrename -h -O -v lon,plon dustdep_mhw2006_${GRID}_${version}.nc
+ncrename -h -O -v lon_bnds,plon_bnds dustdep_mhw2006_${GRID}_${version}.nc
+ncrename -h -O -v lat_bnds,plat_bnds dustdep_mhw2006_${GRID}_${version}.nc
+ncatted -a bounds,plat,o,c,"plat_bnds" dustdep_mhw2006_${GRID}_${version}.nc
+ncatted -a bounds,plon,o,c,"plon_bnds" dustdep_mhw2006_${GRID}_${version}.nc
+
+rm gridtmp.nc
+rm weights_${GRID}.nc
+rm gridfile_${GRID}.nc 
