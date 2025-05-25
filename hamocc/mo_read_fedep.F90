3d309a85173105e35763b99fc422c57600891459
Mariana Vertenstein <marianav@met.no>
2025-05-25 20:22:25 +0200

    generalization of dust deposition for new source

--- hamocc/mo_read_fedep.F90
+++ hamocc/mo_read_fedep.F90
@@ -114,21 +114,21 @@ contains
       case('mahw2006')
         ! Dust/iron deposition using the Mahowald et al. 2006 model data.
         ! The variable 'DUST' contains the total dust-flux in kg/m2/month; this
-        ! is converted to kmol fe/m2/s of bioavailable (soluble) iron using the molar 
-        ! weight of iron (mw_fe=55.85) and assuming that dust contains a fraction 
-        ! 'frac_ironindust' of iron, and that soluble iron is a fraction 'frac_soliron' of 
+        ! is converted to kmol fe/m2/s of bioavailable (soluble) iron using the molar
+        ! weight of iron (mw_fe=55.85) and assuming that dust contains a fraction
+        ! 'frac_ironindust' of iron, and that soluble iron is a fraction 'frac_soliron' of
         ! total iron. A tuning factor 'fetune' is applied to the soluble fraction of iron.
-        ! Note the conversion kg/m2/month -> kg/m2/s is assuming 30 days per month.	  
+        ! Note the conversion kg/m2/month -> kg/m2/s is assuming 30 days per month.
         call read_netcdf_var(ncid,'DUST',dustflx_tot(1,1,1),12,0,0)
         dustflx_tot(:,:,:) = dustflx_tot(:,:,:)/30.0/sec_per_day
         dustflx_sfe(:,:,:) = dustflx_tot(:,:,:)*frac_ironindust*frac_soliron/mw_fe*fetune
 
       case('GESAMP2018')
         ! Iron deposition using data from the GESAMP atmospheric iron deposition
-        ! model intercomparison study (Myriokefalitakis et al. 2018, 
+        ! model intercomparison study (Myriokefalitakis et al. 2018,
         ! doi:10.5194/bg-15-6659-2018).
-        ! The variables 'TFe' and 'LFe' contain the total and soluble (labile) iron 
-        ! fluxes in kg/m2/s; soluble iron is converted to kmol Fe/m2/s using the molar weight of   
+        ! The variables 'TFe' and 'LFe' contain the total and soluble (labile) iron
+        ! fluxes in kg/m2/s; soluble iron is converted to kmol Fe/m2/s using the molar weight of
         ! iron (mw_fe=55.85). A tuning factor 'fetune' is applied to the soluble fraction of iron.
         ! The total dust-flux is 'back-calculated' from total iron assuming a fixed fraction
         ! of iron in dust.
@@ -195,26 +195,35 @@ contains
     integer,        intent(in)  :: kbnd                   ! nb of halo-rows
     integer,        intent(in)  :: kplmon                 ! current month.
     real,           intent(out) :: dust(kpie,kpje,ndust)  ! dust/sFe flux for current month
-    real, optional, intent(in)  :: dust_stream(1-kbnd:kpie+kbnd,1-kbnd:kpje+kbnd)
-    !real, optional, intent(in)  :: dust_stream(1-kbnd:kpie+kbnd,1-kbnd:kpje+kbnd,ndust)
+    real, optional, intent(in)  :: dust_stream(1-kbnd:kpie+kbnd,1-kbnd:kpje+kbnd,ndust)
 
-    integer :: i,j
+    integer :: i,j,n
     logical :: debug = .true.
     logical :: first_time = .true.
 
     if (present(dust_stream)) then
-    
+
       select case(trim(fedep_source))
 
         case('mahw2006')
           do j=1,kpje
             do i=1,kpie
               ! note that the stream is read from a file where units already are in kg/m2/s
-              dust(i,j,itdust) = dust_stream(i,j)  
-              dust(i,j,isfe)   = dust_stream(i,j)*frac_ironindust*frac_soliron/mw_fe*fetune
+              dust(i,j,itdust) = dust_stream(i,j,1)
+              dust(i,j,isfe)   = dust_stream(i,j,1)*frac_ironindust*frac_soliron/mw_fe*fetune
             end do
           end do
-       
+
+        case('GESAMP2018')
+          do
+          do j=1,kpje
+            do i=1,kpie
+              ! note that the stream is read from a file where units already are in kg/m2/s
+              dust(i,j,itdust) = dust_stream(i,j,1)
+              dust(i,j,isfe)   = dust_stream(i,j,2)*frac_ironindust*frac_soliron/mw_fe*fetune
+            end do
+          end do
+
         case default
           if (mnproc==1) then
             call xchalt('(get_fedep: invalid fedep_source for dust stream)')
@@ -222,7 +231,7 @@ contains
           end if
 
       end select
-       
+
     else
       dust(:,:,itdust) = dustflx_tot(:,:,kplmon)
       dust(:,:,isfe)   = dustflx_sfe(:,:,kplmon)
