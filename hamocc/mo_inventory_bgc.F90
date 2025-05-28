664b5cb0da29f6d44df2262ff77155a9a687708c
TimotheeBrgs <timothee.bourgeois@norceresearch.no>
2025-05-28 10:15:21 +0200

    Fix stoichiometric ratios of riverine labile DOC for inventories

--- hamocc/mo_inventory_bgc.F90
+++ hamocc/mo_inventory_bgc.F90
@@ -481,10 +481,10 @@ contains
              &                   - srivflux(iralk) ! no DIN & DIP substraction because alkalinity
                                                    ! changes due to instantaneous remineralisation 
                                                    ! of riverine DOC are ignored
-        totalnitr   = totalnitr  - srivflux(irdoc)*rnit-srivflux(irtdoc)*rnit_tdochc               &
+        totalnitr   = totalnitr  - (srivflux(irdoc)+srivflux(irtdoc))*rnit_tdochc                  &
              &                   - srivflux(irdet)*rnit_tdoclc - srivflux(irdin)
         totalphos   = totalphos  - srivflux(irdoc)-srivflux(irtdoc)-srivflux(irdet)-srivflux(irdip)
-        totaloxy    = totaloxy   - srivflux(irdoc)*(-24.) - srivflux(irtdoc)*(-49.5)               &
+        totaloxy    = totaloxy   - (srivflux(irdoc)+srivflux(irtdoc))*(-49.5)                      &
              &                   - srivflux(irdet)*(-10.5)                                         &
              &                   - srivflux(irdin)*1.5 - srivflux(irdip)*2.                        &
              &                   - (srivflux(iralk)+srivflux(irdin)+srivflux(irdip))
